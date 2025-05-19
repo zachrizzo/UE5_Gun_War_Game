a6b36ae960a9c1643bc547fda9fd507200531c03
zachrizzo <zachcilwa@gmail.com>
2025-05-19 01:05:48 -0700

    Update: Add Saved directory files (with LFS)

---
+++ Saved/ShaderDebugInfo/PCD3D_SM6/DG_Zombie_SeparatedBodies/UpdateGraph$Setup_ComputeGraph/TestingDeformation_OptimusNode_CustomComputeKernel/FComputeKernelShader/0/CompileDXC.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET DXC="D:\Program Files\GAMES\UE_5.5\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+IF NOT EXIST %DXC% (
+	ECHO Couldn't find dxc.exe under "D:\Program Files\GAMES\UE_5.5\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	GOTO :END
+)
+%DXC% -HV 2018 -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T cs_6_6 -E TestingDeformation_OptimusNode_CustomComputeKernel -Fc ComputeKernel.d3dasm -Fo ComputeKernel.dxil ComputeKernel.usf
+:END
+PAUSE
